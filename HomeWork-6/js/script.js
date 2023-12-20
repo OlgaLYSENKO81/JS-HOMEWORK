0b7da69d941d253b041a1cb51bdf7a9530aad298
OlgaLYSENKO81 <142930531+OlgaLYSENKO81@users.noreply.github.com>
2023-12-20 22:40:56 +0200

    Add files via upload

---
+++ HomeWork-6/js/script.js
@@ -0,0 +1,38 @@
+// Створити телефонну книгу
+// -створити початковий клас Abonent, де зберігатимуться ім*я і номер
+// -створити set який прийматиме телефон і номер
+// -створити get який виводитиме данні про абонента
+// -створити три різних юзери
+// -вивести данні
+
+// class Abonent {
+//   constructor(name, number) {
+//     this.name = 'name';
+//     this.number = 'number';
+//   }
+//   firstName = '';
+//   lastName = '';
+//   firstelephone = '';
+//   lasttelephone = '';
+
+//   set name(newName) {
+//     const user = newName.split(' ');
+//     this.firstName = user[0];
+//     this.firstelephone = user[0];
+//     this.lastName = user[1];
+//     this.lasttelephone = user[1];
+//   }
+
+//   get name() {
+//     return `first name : ${this.firstName} first telephone : ${this.firsttelephone} last name : ${this.lastName} last telephone : ${this.lasttelephone}
+//     }`;
+//   }
+// }
+
+// const user1 = (Abonent.name = 'Антон Іванов, телефон 0952453256');
+// const user2 = (Abonent.name = 'Максим Чурсін, телефон 0965478521');
+// const user3 = (Abonent.name = 'Катерина Максименко, телефон 0639652147');
+
+// console.log(user1);
+// console.log(user2);
+// console.log(user3);
