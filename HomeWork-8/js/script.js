73eb5598f52b02ce8d5b35472c54ed0628d620fa
OlgaLYSENKO81 <142930531+OlgaLYSENKO81@users.noreply.github.com>
2023-12-23 21:39:21 +0200

    Add files via upload

---
+++ HomeWork-8/js/script.js
@@ -0,0 +1,13 @@
+//                        ---1---
+// Напишіть код, щоб вибрати елемент з атрибутом data - widget - name з документа
+// та прочитати його значення.
+// <!DOCTYPE html>
+// <html>
+// <body>
+// <div data-widget-name="menu">Виберіть жанр</div>
+// <script>/* ваш код */</script>
+// </body>
+// </html>
+
+// let elem = document.querySelector('[data-widget-name]');
+// console.log(elem.getAttribute('data-widget-name'));
