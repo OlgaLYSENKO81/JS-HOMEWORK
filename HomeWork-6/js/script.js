5972e6cb7c5322d01ebf9319bc8895c0edb4aff6
OlgaLYSENKO81 <142930531+OlgaLYSENKO81@users.noreply.github.com>
2023-12-23 19:02:04 +0200

    Add files via upload

--- HomeWork-6/js/script.js
+++ HomeWork-6/js/script.js
@@ -6,33 +6,45 @@
 // -вивести данні
 
 // class Abonent {
-//   constructor(name, number) {
-//     this.name = 'name';
-//     this.number = 'number';
+//   constructor(object) {
+//     this.name = object.name;
+//     this.telNumber = object.telNumber;
 //   }
+
 //   firstName = '';
 //   lastName = '';
-//   firstelephone = '';
-//   lasttelephone = '';
+//   telNumber = Number;
 
 //   set name(newName) {
 //     const user = newName.split(' ');
 //     this.firstName = user[0];
-//     this.firstelephone = user[0];
 //     this.lastName = user[1];
-//     this.lasttelephone = user[1];
+//     this.telNumber = Number;
 //   }
 
 //   get name() {
-//     return `first name : ${this.firstName} first telephone : ${this.firsttelephone} last name : ${this.lastName} last telephone : ${this.lasttelephone}
-//     }`;
+//     return `First name : ${this.firstName}  last name : ${this.lastName}  telephone : ${this.telNumber}
+//       }`;
 //   }
 // }
 
-// const user1 = (Abonent.name = 'Антон Іванов, телефон 0952453256');
-// const user2 = (Abonent.name = 'Максим Чурсін, телефон 0965478521');
-// const user3 = (Abonent.name = 'Катерина Максименко, телефон 0639652147');
+// const Anton = new Abonent({
+//   name: 'Антон Іванов',
+//   telNumber: '0952453256',
+// });
 
-// console.log(user1);
-// console.log(user2);
-// console.log(user3);
+// const Max = new Abonent({
+//   name: 'Максим Чурсін',
+//   telNumber: '0965478521',
+// });
+
+// const Kate = new Abonent({
+//   name: 'Катерина Максименко',
+//   telNumber: '0639652147',
+// });
+
+// console.group('Phone book');
+// console.log(Anton);
+// console.log(Max);
+// console.log(Kate);
+// console.groupEnd();
