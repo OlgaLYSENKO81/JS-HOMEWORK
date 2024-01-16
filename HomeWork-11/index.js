7347e7f27efeed68c020aa58efa36b4f350492c6
OlgaLYSENKO81 <142930531+OlgaLYSENKO81@users.noreply.github.com>
2024-01-16 19:34:39 +0200

    Add files via upload

---
+++ HomeWork-11/index.js
@@ -0,0 +1,13 @@
+// import { name } from './module';
+// import './style.css';
+
+// // console.log('Your name: ' + name);
+
+// import isEmail from 'validator/lib/isEmail';
+// import isURL from 'validator/lib/isURL';
+// import toDate from 'validator/lib/toDate';
+// console.log(isEmail('foo@Bar.com'));
+// console.log(isURL('https://lms.human.ua/app/calendar'));
+// console.log(toDate('12.12.2000'));
+var moment = require('moment');
+console.log(moment().format('MMM Do YY'));
